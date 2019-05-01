827a493fe0dc0e84759eb2a6a07125bac5f1aee4
SHYLA <sheilabernicew@gmail.com>
2019-05-02 01:30:24 +0300

    add modified  changes

--- src/app/goal-details/goal-details.component.ts
+++ src/app/goal-details/goal-details.component.ts
@@ -10,7 +10,10 @@ export class GoalDetailsComponent implements OnInit {
 
   @Input() goal:Goal;
   @Output() isComplete= new EventEmitter<boolean>();
-
+  
+  goalComplete(complete:boolean)  {
+    this.isComplete.emit(complete);
+  }
   goalDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,13 +1,7 @@
 export class Goal {
-  static push(goal: any): any {
-    throw new Error("Method not implemented.");
-  }
-  static splice(index: any, arg1: number): any {
-    throw new Error("Method not implemented.");
-  }
-    public showDescription:boolean;
-    constructor(public id:number, public name:string,public description:string ,public completeDate ){
-        this.showDescription=false
+  public showDescription:boolean;
+  constructor(public id:number, public name:string,public description:string ,public completeDate:Date){
+      this.showDescription=false
 
-    }
+  }
 }
